c389e4a9594ca694c007433b11c0d81800a2221f
RVinicius <ruza.vinicius@aluno.ifsp.edu.br>
2026-07-29 13:16:26 +0000

    Enviando projeto do flasky para o repo AvalicaoContinua1

--- AvalicaoContinua1/flask_app.py
+++ AvalicaoContinua1/flask_app.py
@@ -1,10 +1,11 @@
+
 # A very simple Flask Hello World app for you to get started with...
+
 from flask import Flask
+
 app = Flask(__name__)
+
 @app.route('/')
 def hello_world():
-    return '<h1>Hello World! <h1> <p>Disiplina PTBDSWS<p>'
+    return 'Hello from Flask! Bem vindos alunos do IFSP'
 
-@app.route('/user/<name>')
-def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
